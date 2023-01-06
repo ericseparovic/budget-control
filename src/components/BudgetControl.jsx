774416b6e779b72533e853b3318908d10e474e45
Eric Separovic <ericseparovic07@gmail.com>
2023-01-06 17:16:59 -0300

    Format budget amount

--- src/components/BudgetControl.jsx
+++ src/components/BudgetControl.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 
 function BudgetControl({ budget }) {
+  const formatQuantity = (count) => {
+    return count.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+  };
   return (
     <div className="bg-slate-50 rounded-xl flex justify-center gap-4 p-6 shadow-xl">
       <div>
@@ -8,13 +14,17 @@ function BudgetControl({ budget }) {
       </div>
       <div className="text-left text-lg">
         <p className="text-purple-700 font-bold">
-          Presupuesto: <span className="text-gray-500">${budget}</span>
+          Presupuesto:{" "}
+          <span className="text-gray-500">{formatQuantity(budget)}</span>
         </p>
         <p className="text-purple-700 font-bold">
-          Disponible: <span className="text-gray-500">$2000</span>
+          Disponible:{" "}
+          <span className="text-gray-500">{formatQuantity(budget)}</span>
         </p>
+
         <p className="text-purple-700 font-bold">
-          Gastado: <span className="text-gray-500">$2000</span>
+          Gastado:{" "}
+          <span className="text-gray-500">{formatQuantity(budget)}</span>
         </p>
       </div>
     </div>
